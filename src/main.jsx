cb0016b7d4be93acb021c54551d9ac002fd51327
gtca5000 <gtca5000@gmail.com>
2026-02-13 13:16:18 +1030

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ValentineGolf from './golf.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ValentineGolf />
+  </React.StrictMode>
+)
